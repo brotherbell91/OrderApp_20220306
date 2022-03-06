26dd9d5f537bbd9742327ae37c22e3972afed88e
이형종 <hj36879@naver.com>
2022-03-06 14:46:43 +0900

    뷰페이저 어댑터 - 상속 / 오버라이딩 문제 해결  - 각 함수 내용 구현 해야함

--- app/src/main/java/com/hyeongjong/orderapp_20220306/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/hyeongjong/orderapp_20220306/fragments/MyProfileFragment.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hyeongjong.orderapp_20220306.R
 
-class PizzaStoreListFragment : Fragment() {
+class MyProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_pizza_stroe_list, container, false)
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -24,4 +24,3 @@ class PizzaStoreListFragment : Fragment() {
 
     }
 }
-
