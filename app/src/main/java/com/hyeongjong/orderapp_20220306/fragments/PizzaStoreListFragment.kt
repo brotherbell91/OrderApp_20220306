10548848ba4f3425eca3585ddd6e2a4fafaafbd3
이형종 <hj36879@naver.com>
2022-03-06 14:42:11 +0900

    피자가게 목록 표현 프래그먼트 생성  - 임시 UI : 문구만 배치

---
+++ app/src/main/java/com/hyeongjong/orderapp_20220306/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,22 @@
+package com.hyeongjong.orderapp_20220306.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hyeongjong.orderapp_20220306.R
+
+class PizzaStoreListFragment : Fragment {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_stroe_list,container,false)
+}
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
