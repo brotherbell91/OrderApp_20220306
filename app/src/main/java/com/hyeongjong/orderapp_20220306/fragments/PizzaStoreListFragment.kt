2e51e282282d74030ce59fd65776f3a33e3a94bf
이형종 <hj36879@naver.com>
2022-03-06 16:01:26 +0900

    getView에서, 데이터/UI 추출, 이름만 우선 연결

--- app/src/main/java/com/hyeongjong/orderapp_20220306/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/hyeongjong/orderapp_20220306/fragments/PizzaStoreListFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hyeongjong.orderapp_20220306.R
+import com.hyeongjong.orderapp_20220306.adapters.StoreAdapter
+import com.hyeongjong.orderapp_20220306.datas.StoreData
+import kotlinx.android.synthetic.main.fragment_pizza_stroe_list.*
 
 class PizzaStoreListFragment : Fragment() {
+
+    val mPizzaStoreList = ArrayList<StoreData>()
+    lateinit var mPizzaStoreAdapter : StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +25,14 @@ class PizzaStoreListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        mPizzaStoreList.add(StoreData("피자헛", 4.5, "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        mPizzaStoreList.add(StoreData("피자헛", 3.0, "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        mPizzaStoreList.add(StoreData("피자헛", 5.0, "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        mPizzaStoreList.add(StoreData("피자헛", 3.5, "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+
+        mPizzaStoreAdapter = StoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStoreList)
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
 
 
 
